c0948da1d7c13ace68b1707e794dc5a3a07f3e31
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-08-18 16:26:38 -0400

    Time: 1 ms (10.26%), Space: 17.8 MB (33.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+
         if n == 1:
             return nums[0]
+        
         if n == 2:
             return max(nums[0], nums[1])
 
@@ -12,7 +14,7 @@ class Solution:
             if i in memo:
                 return memo[i]
             else:
-                memo[i ] = max(dp(i - 1), dp(i - 2) + nums[i])
+                memo[i] = max(dp(i - 1), dp(i - 2) + nums[i])
                 return memo[i]
 
         return dp(n - 1)
