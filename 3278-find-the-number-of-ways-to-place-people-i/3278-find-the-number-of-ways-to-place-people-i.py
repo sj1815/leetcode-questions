0eacba6667dca154760dca80000b7c8e9f28998a
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-09-03 16:02:07 -0700

    Time: 11 ms (92.61%), Space: 17.6 MB (98.53%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
@@ -1,16 +1,18 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
         points.sort(key=lambda p: (p[0], -p[1]))
-        ans = 0
-        
+        count = 0
+
         for i in range(len(points)):
             yi = points[i][1]
             max_y = float("-inf")
             for j in range(i + 1, len(points)):
                 yj = points[j][1]
                 if yi >= yj > max_y:
-                    ans += 1
+                    count += 1
                     max_y = yj
-        return ans
+        
+        return count
+
 
         
