bfaf6f4313112c87bc210038dc936a77fbfd800d
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-09-22 15:25:38 -0700

    Time: 7 ms (82.02%), Space: 19.1 MB (16.59%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -1,14 +1,9 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        # # Step 1: Sort all visits by timestamp
-        # visits = sorted(zip(timestamp, username, website))
-        
-        # Step 2: Group websites by user
         user_visits = defaultdict(list)
         for t, u, w in sorted(zip(timestamp, username, website)):
             user_visits[u].append(w)
         
-        # Step 3: Generate unique 3-sequence patterns per user
         pattern_users = defaultdict(set)
         for user, sites in user_visits.items():
             n = len(sites)
@@ -21,6 +16,5 @@ class Solution:
                             seen.add(pattern)
                             pattern_users[pattern].add(user)
         
-        # Step 4: Find the pattern with max users, lexicographically smallest
         return min(pattern_users.keys(), key=lambda p: (-len(pattern_users[p]), p))
         
