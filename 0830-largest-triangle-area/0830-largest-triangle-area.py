afeac70533446329f46976e338da7dd77ecfd898
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-09-26 17:16:35 -0700

    Time: 52 ms (40.34%), Space: 17.9 MB (30.76%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.py
+++ 0830-largest-triangle-area/0830-largest-triangle-area.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
 
-        def shoe_lace(p1, p2, p3):
+        def area(p1, p2, p3):
             return 0.5 * abs(
                 p1[0] * (p2[1] - p3[1]) +
                 p2[0] * (p3[1] - p1[1]) +
@@ -14,6 +14,6 @@ class Solution:
         for i in range(n):
             for j in range(i + 1, n):
                 for k in range(j + 1, n):
-                    max_area = max(max_area, shoe_lace(points[i], points[j], points[k]))
+                    max_area = max(max_area, area(points[i], points[j], points[k]))
         return max_area
         
