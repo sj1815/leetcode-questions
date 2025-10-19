ddecdf5ac1d6fc1f41b53ec9b7c3d864c5d40a56
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-10-18 17:20:25 -0700

    Time: 617 ms (60.43%), Space: 26.8 MB (35.25%) - LeetHub

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLexSmallestString(self, s: str, a: int, b: int) -> str:
+        res,q = set(),deque([tuple(map(int,s))])
+        while q:
+            t = q.pop()
+            for t in tuple((d+a)%10 for d,a in zip(t,cycle((0,a)))),t[-b:]+t[:-b]:
+                if t not in res: res.add(t);q.append(t)
+        
+        return ''.join(map(str,min(res)))
+        
