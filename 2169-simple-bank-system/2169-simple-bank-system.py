c7bfa6e869674ab754fe32d4a767f7b40cc891e7
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-10-27 08:47:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.py
+++ 2169-simple-bank-system/2169-simple-bank-system.py
@@ -5,31 +5,14 @@ class Bank:
         self.n = len(balance)   
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if not self._valid(account1) or not self._valid(account2):
-            return False
-        if self.balance[account1 - 1] < money:
-            return False
-        self.balance[account1 - 1] -= money
-        self.balance[account2 - 1] += money
-        return True
+        
 
     def deposit(self, account: int, money: int) -> bool:
-        if not self._valid(account):
-            return False
-        self.balance[account - 1] += money
-        return True
         
 
     def withdraw(self, account: int, money: int) -> bool:
-        if not self._valid(account):
-            return False
-        if self.balance[account - 1] < money:
-            return False
-        self.balance[account - 1] -= money
-        return True
-
-    def _valid(self, account: int) -> bool:
-        return 1 <= account <= self.n
+    
+    def valid(self, )
         
 
 
