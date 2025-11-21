f2b1593888be9e91a93950f8a4bb111dc88f37ea
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-11-21 12:52:14 +0530

    Time: 4036 ms (58.53%), Space: 571.4 MB (52.09%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
@@ -6,14 +6,11 @@ class Solution:
         @cache
         def dfs(cx, cy, direction, turn, target):
             nx, ny = cx + DIRS[direction][0], cy + DIRS[direction][1]
-            # If it goes beyond the boundary or the next node's value is not the target value, then return
             if nx < 0 or ny < 0 or nx >= m or ny >= n or grid[nx][ny] != target:
                 return 0
             turn_int = 1 if turn else 0
-            # Continue walking in the original direction.
             max_step = dfs(nx, ny, direction, turn, 2 - target)
             if turn:
-                # Clockwise rotate 90 degrees turn
                 max_step = max(
                     max_step,
                     dfs(nx, ny, (direction + 1) % 4, False, 2 - target),
