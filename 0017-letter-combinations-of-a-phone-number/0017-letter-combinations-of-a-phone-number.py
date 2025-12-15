92f1342a4917cee2b1c9b70e7d26b3f85bddbd84
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-12-15 12:42:17 +0530

    Time: 0 ms (100%), Space: 18 MB (22.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,9 +2,9 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits == '':
             return []
-        
+
         n = len(digits)
-        ans, res = [], []
+        res, ans = [], []
         letter_map = {
             "2" : "abc",
             "3" : "def",
@@ -19,19 +19,17 @@ class Solution:
         def backtrack(i = 0):
             if i == n:
                 ans.append(''.join(res))
-                return
-            
+                return 
+
             for letter in letter_map[digits[i]]:
                 res.append(letter)
                 backtrack(i + 1)
                 res.pop()
 
+        
         backtrack(0)
         return ans
 
 
-
         
-
-
         
