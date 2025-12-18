f53fbaf087559e840f228558bb0496cecde3cc54
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-12-18 12:56:10 +0530

    Time: 3 ms (83.53%), Space: 19.3 MB (50.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts =  Counter(nums)
+        counts = Counter(nums)
         return max(counts.keys(), key=counts.get)
