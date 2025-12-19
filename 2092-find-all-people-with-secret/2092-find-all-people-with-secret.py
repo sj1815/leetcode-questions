064b5f3532d8c5cdc1a8aad290e16b1cec19b512
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2025-12-19 11:28:41 +0530

    Time: 373 ms (38.22%), Space: 61.7 MB (68.15%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        meetings.sort(key=lambda x: x[2])
+
+        same_time_meetings = defaultdict(list)
+        for x, y, t in meetings:
+            same_time_meetings[t].append((x, y))
+
+        knows_secret = [False] * n
+        knows_secret[0] = True
+        knows_secret[firstPerson] = True
+
+        for t in same_time_meetings:
+            meet = defaultdict(list)
+            for x, y in same_time_meetings[t]:
+                meet[x].append(y)
+                meet[y].append(x)
+
+            q = set()
+            for x, y in same_time_meetings[t]:
+                if knows_secret[x]:
+                    q.add(x)
+                if knows_secret[y]:
+                    q.add(y)
+
+            q = deque(q)
+            while q:
+                person = q.popleft()
+                for next_person in meet[person]:
+                    if not knows_secret[next_person]:
+                        knows_secret[next_person] = True
+                        q.append(next_person)
+
+        return [i for i in range(n) if knows_secret[i]]
