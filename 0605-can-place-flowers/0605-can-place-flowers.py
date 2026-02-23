ea73eef5b097435d9e7b786eda3948b106ad1525
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-02-23 14:08:16 +0530

    Time: 6 ms (82%), Space: 19.8 MB (24.05%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(m):
             if flowerbed[i] == 0:
                 left_check = (i == 0) or (flowerbed[i - 1] == 0)
-                right_check = (i == m - 1) or (flowerbed[i + 1] == 0)
+                right_check = (i == m - 1 ) or (flowerbed[i + 1] == 0)
                 if left_check and right_check:
                     flowerbed[i] = 1
                     n -= 1
@@ -13,6 +13,3 @@ class Solution:
                         return True
 
         return n <= 0
-                
-
-        
