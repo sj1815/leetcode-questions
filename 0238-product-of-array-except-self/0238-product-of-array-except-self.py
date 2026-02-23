b9af22267cf148fa686083e36694b7daed1ffc5d
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-02-23 15:50:19 +0530

    Time: 23 ms (57.91%), Space: 26.8 MB (27.3%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,7 +4,7 @@ class Solution:
         l_mult = 1
         r_mult = 1
         l_arr = [0] * n
-        r_arr = [0] * n
+        r_arr = [0] * n 
 
         for i in range(n):
             j = - i - 1
@@ -12,6 +12,5 @@ class Solution:
             r_arr[j] = r_mult
             l_mult *= nums[i]
             r_mult *= nums[j]
-        
-        return [l * r for l, r in zip(l_arr, r_arr)]
 
+        return [l * r for l, r in zip(l_arr, r_arr)]  
