9c912898076e13fd22226f167dce0c7a9aebcad5
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-05-25 20:45:01 +0530

    Time: 219 ms (25.44%), Space: 24.6 MB (14.81%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        f, pre = [0] * n, [0] * n
+        f[0] = 1
+        for i in range(minJump):
+            pre[i] = 1
+        for i in range(minJump, n):
+            left, right = i - maxJump, i - minJump
+            if s[i] == "0":
+                total = pre[right] - (0 if left <= 0 else pre[left - 1])
+                f[i] = int(total != 0)
+            pre[i] = pre[i - 1] + f[i]
+
+        return bool(f[n - 1])
