6686d33e0246b508edf576b8138e78cfea3425fe
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-06-07 11:15:23 +0530

    Time: 139 ms (56.5%), Space: 27.1 MB (72.56%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        node_map = {}
+        children = set()
+        
+        for description in descriptions:
+            parent_value = description[0]
+            child_value = description[1]
+            is_left = bool(description[2])
+
+            if parent_value not in node_map:
+                node_map[parent_value] = TreeNode(parent_value)
+            if child_value not in node_map:
+                node_map[child_value] = TreeNode(child_value)
+
+            if is_left:
+                node_map[parent_value].left = node_map[child_value]
+            else:
+                node_map[parent_value].right = node_map[child_value]
+
+            children.add(child_value)
+
+        for node in node_map.values():
+            if node.val not in children:
+                return node
+
+        return None
