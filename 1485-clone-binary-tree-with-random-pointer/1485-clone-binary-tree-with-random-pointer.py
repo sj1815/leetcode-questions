a4d28e0c0cb6a8cdd6e9fc5ecb05fb09791ab296
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-06-27 11:41:56 +0530

    Time: 137 ms (59.4%), Space: 24.6 MB (94.98%) - LeetHub

---
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -0,0 +1,37 @@
+# Definition for Node.
+# class Node:
+#     def __init__(self, val=0, left=None, right=None, random=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.random = random
+
+class Solution:
+    def __init__(self):
+        # Hashmap to map old tree's nodes with new tree's nodes.
+        self.seen: dict[str, int] = {None: None}
+            
+    # Function to traverse on the sub graph of 'root'.
+    def dfs(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
+        if not root:
+            return None
+
+        if self.seen.get(root) is not None:
+            return self.seen.get(root)
+
+        new_root = NodeCopy(root.val)
+        # Mark old root as seen and store its respective new node.
+        self.seen[root] = new_root
+        
+        # Traverse on all 3 edges of root and attach respective new node
+        # to the newRoot.
+        new_root.left = self.dfs(root.left)
+        new_root.right = self.dfs(root.right)
+        new_root.random = self.dfs(root.random)
+        return new_root
+
+    def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
+        # Traverse on each node of the given tree.
+        new_root = self.dfs(root)
+        return new_root
+        
