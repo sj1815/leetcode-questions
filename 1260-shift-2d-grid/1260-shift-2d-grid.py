9ac9313d00e242e37c2068561fdf942d572c70e0
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-07-20 19:43:19 +0530

    Time: 115 ms (17.28%), Space: 19.5 MB (81.69%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,12 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        num_rows, num_cols = len(grid), len(grid[0])
+
+        for _ in range(k):
+            previous = grid[-1][-1]
+            for row in range(num_rows):
+                for col in range(num_cols):
+                    temp = grid[row][col]
+                    grid[row][col] = previous
+                    previous = temp
+        return grid
