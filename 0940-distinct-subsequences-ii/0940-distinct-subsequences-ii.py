78aa8b3e94f5fe744bc04471b72a39143be07c42
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2026-09-07 09:10:09 +0530

    Time: 5 ms (94.27%), Space: 19.3 MB (72.4%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10 ** 9 + 7
+        dp = [0] * 26
+        total = 0
+
+        for ch in s:
+            i = ord(ch) - ord('a')
+            new = (total + 1) % MOD
+            total = (total + new - dp[i]) % MOD
+            dp[i] = new
+
+        return total
